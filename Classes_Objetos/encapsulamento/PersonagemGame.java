9367fb959cb69e9aeaa288c4b35671e881c603eb
hugo <jefferson.luiz.cruz@gmail.com>
2025-07-24 21:24:41 -0300

    copia e cola

--- Classes_Objetos/encapsulamento/PersonagemGame.java
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -17,5 +17,19 @@ public class PersonagemGame {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public void tomarDano(int quantidadeDeDano) {
+        saudeAtual -= quantidadeDeDano;
+        if (saudeAtual < 0) {
+            saudeAtual = 0;
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        saudeAtual += quantidadeDeCura;
+        if (saudeAtual > 100) {
+            saudeAtual = 100;
+        }
+    }
 }
 
